0714447d81b4e7a688122db71448994b1870547d
tsiotaku <tsiotaku@gmail.com>
2016-11-17 23:21:50 +0800

    後台管理員密碼修改及Validation驗證 新增傳參數blade樣板寫法

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -46,6 +46,25 @@ class IndexController extends Controller
                 }else{
                     $validator->errors()->add('pw_error', '原密碼輸入錯誤');
                     return back()->withErrors($validator);
+
+                    /*寫法二
+                     return back()->with('errors','原密碼輸入錯誤');
+                    ---pass.blade.php---
+                    使用with傳參數用@foreach($errors->all() as $error)會出錯
+                    因為這時的$errors為字串非陣列，直接輸出{{ $errors }}即可
+                    有使用withErrors和with兩種方式傳參數需要先判斷$errors是否為object
+                    @if(count($errors) >0 )
+                        <div class="mark">
+                            @if(is_object($errors))
+                                @foreach($errors->all() as $error)
+                                    <p>{{ $error }}</p>
+                                @endforeach
+                            @else
+                                <p>{{ $errors }}</p>
+                            @endif
+                        </div>
+                    @endif
+                     */
                 }
             }else{
                 //dd($validator->errors()->all());
