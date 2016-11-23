b1cbfded7246a61c6cc75be84baba7da0833168a
tsiotaku <tsiotaku@gmail.com>
2016-11-23 17:19:23 +0800

    後台"友情連結"新增、修改功能完成

--- app/Http/Controllers/Admin/LinkController.php
+++ app/Http/Controllers/Admin/LinkController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
 
 class LinkController extends Controller
 {
@@ -19,6 +20,51 @@ class LinkController extends Controller
 
     public function show(){
         
+    }
+
+    public function create()
+    {
+        return view('admin.link.add');
+    }
+
+    public function store()
+    {
+        $input = Input::except('_token');
+        $rules = [
+            'link_name' =>'required',
+            'link_url' => 'required',
+        ];
+        $msg = [
+            'link_name.required' => '名稱不能為空',
+            'link_url.required' => '網址不能為空',
+        ];
+
+        $validator = Validator::make($input,$rules,$msg);
+
+        if($validator->passes()){
+            $re = Link::create($input);
+            if($re){
+                return redirect('admin/link')->with('msg','新增成功');
+            }else{
+                $validator->errors()->add('add_error', '添加失敗');
+                return back()->withErrors($validator);
+            }
+        }else{
+            return back()->withErrors($validator);
+        }
+    }
+
+    public function edit($id)
+    {
+        $datas = Link::find($id);
+        return view('admin.link.edit',compact('datas'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $input = Input::except('_token','_method'); //使用except()排除_token、_method欄位
+        Link::where('link_id',$id)->update($input);
+        return  redirect('admin/link')->with('msg','修改成功');
     }
 
     public function changeOrder(){
